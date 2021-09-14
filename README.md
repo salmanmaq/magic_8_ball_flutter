603ce6b596b55cee01c41f5605fa72e4f69b812f
Salman Maqbool <salmanmaq@gmail.com>
2021-09-15 02:24:16 +0500

    Working app

---
+++ README.md
@@ -0,0 +1,3 @@
+# magic_8_ball
+
+Let a random number generator decide your course
---
+++ android/app/src/main/kotlin/ai/psychiatr/magic_8_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package ai.psychiatr.magic_8_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
